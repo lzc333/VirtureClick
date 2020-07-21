f64a2cf80865024f44d3859f8699f5f12f867c43
zhichengliu <zhichengliu@pateo.com.cn>
2020-07-21 10:47:56 +0800

    优化体验

--- app/src/main/java/com/test/virtureclick/tools/Ext.kt
+++ app/src/main/java/com/test/virtureclick/tools/Ext.kt
@@ -2,9 +2,10 @@ package com.test.virtureclick.tools
 
 import android.util.Log
 import android.util.Patterns
+import android.view.View
 import java.util.regex.Pattern
 
-const val TAG = "Liuzhicheng"
+const val TAG = "liuzhicheng"
 
 fun String.d(tag: String = TAG) {
     Log.d(tag, this)
@@ -14,3 +15,33 @@ fun String.trimBrackets() =
  this.replace("[","").replace("]","")
 
 
+fun <T : View> T.clickWithTrigger(time: Long = 600, block: (T) -> Unit) {
+    triggerDelay = time
+    setOnClickListener {
+        if (clickEnable()) {
+            block(it as T)
+        }
+    }
+}
+
+private fun <T : View> T.clickEnable(): Boolean {
+    var flag = false
+    val currentClickTime = System.currentTimeMillis()
+    if (currentClickTime - triggerLastTime >= triggerDelay) {
+        flag = true
+    }
+    triggerLastTime = currentClickTime
+    return flag
+}
+
+private var <T : View> T.triggerLastTime: Long
+    get() = if (getTag(1123460103) != null) getTag(1123460103) as Long else -601
+    set(value) {
+        setTag(1123460103, value)
+    }
+
+private var <T : View> T.triggerDelay: Long
+    get() = if (getTag(1123461123) != null) getTag(1123461123) as Long else 600
+    set(value) {
+        setTag(1123461123, value)
+    }
