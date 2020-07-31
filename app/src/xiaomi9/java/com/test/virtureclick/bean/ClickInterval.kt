f5f36775c99c514029332efae4ef74cfec98d369
zhichengliu <zhichengliu@pateo.com.cn>
2020-07-31 11:30:44 +0800

    调整小米9点击时间间隔

--- app/src/xiaomi9/java/com/test/virtureclick/bean/ClickInterval.kt
+++ app/src/xiaomi9/java/com/test/virtureclick/bean/ClickInterval.kt
@@ -1,10 +1,10 @@
 package com.test.virtureclick.bean
 
 enum class ClickInterval(val timeMillis: Long) {
-    heartstone_next_battle(350),
-    heartstone_surrender_setting(200),
+    heartstone_next_battle(380),
+    heartstone_surrender_setting(250),
     heartstone_surrender_animation(5000),
-    heartstone_surrender_battle(350),
+    heartstone_surrender_battle(380),
     heartstone_hangup(200),
     heartstone_hangup_battle(3500),
     heartstone_hangup_end_round(7330),
